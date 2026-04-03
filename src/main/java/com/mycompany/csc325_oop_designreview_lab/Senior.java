add2f835f15af4f853e06392ed63a5158ae1b4d2
kaleysantonocito <kaleysantonocito31@gmail.com>
2026-04-03 16:29:38 -0400

    creates senior class

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Senior.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Senior.java
@@ -3,5 +3,13 @@ package com.mycompany.csc325_oop_designreview_lab;
 public class Senior extends Student{
     public Senior(String name, short age, int credits) {
         super(name, age, credits);
+
+        if (credits < 85) {
+            throw new IllegalArgumentException("Senior must have at least 85 credits");
+        }
+    }
+    @Override
+    public String toString() {
+        return "Senior student: " + getName() + ", age: " + getAge() + ", credits: " + getCredits() + ", GPA: " + getGpa();
     }
 }
