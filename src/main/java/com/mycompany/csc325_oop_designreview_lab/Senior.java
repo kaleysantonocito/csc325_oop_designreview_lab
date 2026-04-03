be079f689e5590fc2811ae5b02adc838b75286cb
kaleysantonocito <kaleysantonocito31@gmail.com>
2026-04-03 16:22:29 -0400

    added to freshman

---
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Senior.java
@@ -0,0 +1,7 @@
+package com.mycompany.csc325_oop_designreview_lab;
+
+public class Senior extends Student{
+    public Senior(String name, short age, int credits) {
+        super(name, age, credits);
+    }
+}
