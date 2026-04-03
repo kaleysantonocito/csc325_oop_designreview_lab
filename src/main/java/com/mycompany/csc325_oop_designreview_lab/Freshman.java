719ad0114d099262f52766d4161abfb1d4ff2f60
kaleysantonocito <kaleysantonocito31@gmail.com>
2026-04-03 16:31:41 -0400

    added comments to senior and freshman classes

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Freshman.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Freshman.java
@@ -1,10 +1,12 @@
 package com.mycompany.csc325_oop_designreview_lab;
 
+// Freshman class inherits from Student
 public class Freshman extends Student {
     public Freshman(String name, short age, int credits) {
         super(name, age, credits);
     }
 
+    //returns freshman into as a string
     @Override
     public String toString() {
         return "Freshman student: " + getName() + ", age: " + getAge() + ", credits: " + getCredits() + ", GPA: " + getGpa();
